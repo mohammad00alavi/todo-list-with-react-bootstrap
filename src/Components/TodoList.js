561744dd0a5e95e6dc33faf6336d8fb212e7b585
mreza <mrez9090@gmail.com>
2020-09-29 16:11:42 +0330

    initial commit

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,72 +1,95 @@
 import React, { useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
-import {Col, Row, Container, ListGroup, Badge} from 'react-bootstrap';
-
+import { Col, Row, Container, ListGroup, Badge } from "react-bootstrap";
 
 export default function TodoList() {
-  const [todo, setTodo] = useState({ todos: [] });
+	const [todo, setTodo] = useState({ todos: [] });
 
-  function create(newTodo) {
-    setTodo({
-      todos: [...todo.todos, newTodo],
-    });
-  }
+	let valueStorage = allStorage();
+	console.log("values:", valueStorage);
 
-  function remove(id) {
-    setTodo({
-      todos: todo.todos.filter((t) => t.id !== id),
-    });
-  }
+	// setTodo({
+	// 	todos: [valueStorage],
+	// });
 
-  function update(id, updatedTask) {
-    const updatedTodos = todo.todos.map((td) => {
-      if (td.id === id) {
-        return { ...td, task: updatedTask };
-      }
-      return td;
-    });
-    setTodo({ todos: updatedTodos });
-  }
+	function allStorage() {
+		var values = [],
+			keys = Object.keys(localStorage),
+			i = keys.length;
 
-  function toggleCompletion(id) {
-    const updatedTodos = todo.todos.map((td) => {
-      if (td.id === id) {
-        return { ...td, completed: !td.completed };
-      }
-      return td;
-    });
-    setTodo({ todos: updatedTodos });
-  }
+		while (i--) {
+			values.push(JSON.parse(localStorage.getItem(keys[i])));
+		}
+		return values;
+	}
 
-  const todos = todo.todos.map((td) => {
-    return (
-      <Todo
-        task={td.task}
-        key={td.id}
-        id={td.id}
-        completed={td.completed}
-        removeTodo={remove}
-        updateTodo={update}
-        toggleTodo={toggleCompletion}
-      />
-    );
-  });
-  return (
-      <Container className="TodoList">
-        <Row className="justify-content-md-center">
-          <Col xs={8}>
-            <h2>Todo List <Badge variant="info">{todos.length}</Badge></h2>
-          </Col>
-        </Row>
-        <TodoForm createTodo={create} />
-        <Row className="justify-content-md-center">
-          <Col xs={8}>
-            <ListGroup as='ul' className='TodosContainer'>
-              {todos}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
-  );
+	function create(newTodo) {
+		setTodo({
+			todos: [...todo.todos, newTodo],
+		});
+		setTimeout(() => {
+			console.log(todo.todos);
+		}, 0);
+		localStorage.setItem(newTodo.id, JSON.stringify(newTodo));
+	}
+
+	function remove(id) {
+		setTodo({
+			todos: todo.todos.filter(t => t.id !== id),
+		});
+	}
+
+	function update(id, updatedTask) {
+		const updatedTodos = todo.todos.map(td => {
+			if (td.id === id) {
+				return { ...td, task: updatedTask };
+			}
+			return td;
+		});
+		setTodo({ todos: updatedTodos });
+	}
+
+	function toggleCompletion(id) {
+		const updatedTodos = todo.todos.map(td => {
+			if (td.id === id) {
+				return { ...td, completed: !td.completed };
+			}
+			return td;
+		});
+		setTodo({ todos: updatedTodos });
+	}
+
+	const todos = todo.todos.map(td => {
+		return (
+			<Todo
+				task={td.task}
+				key={td.id}
+				id={td.id}
+				completed={td.completed}
+				removeTodo={remove}
+				updateTodo={update}
+				toggleTodo={toggleCompletion}
+			/>
+		);
+	});
+	return (
+		<Container className="TodoList">
+			<Row className="justify-content-md-center">
+				<Col xs={8}>
+					<h2>
+						Todo List <Badge variant="info">{todos.length}</Badge>
+					</h2>
+				</Col>
+			</Row>
+			<TodoForm createTodo={create} />
+			<Row className="justify-content-md-center">
+				<Col xs={8}>
+					<ListGroup as="ul" className="TodosContainer">
+						{todos}
+					</ListGroup>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
